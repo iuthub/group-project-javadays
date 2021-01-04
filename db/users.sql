ad4780af0ca1c61b99ec93be4c7d87907bf54d9d
Asadbek Khasanov <45283164+khasanovasad@users.noreply.github.com>
2021-01-04 16:18:42 +0500

    fixed typos

--- db/users.sql
+++ db/users.sql
@@ -9,13 +9,13 @@ CREATE TABLE Users (
 )
 
 -- Students
-INSERT INTO lms.Users(userId, password, firstName, lastName, role) VALUES ('U1910000', 'student', 'Pink', 'Floyd', 2)
-INSERT INTO lms.Users(userId, password, firstName, lastName, role) VALUES ('U1910001', 'student', 'Deep', 'Purple', 2)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1910000', 'student', 'Pink', 'Floyd', 2)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1910001', 'student', 'Deep', 'Purple', 2)
 
 -- Admins
-INSERT INTO lms.Users(userId, password, firstName, lastName, role) VALUES ('U1410000', 'admin', 'Foo', 'Fighters', 0)
-INSERT INTO lms.Users(userId, password, firstName, lastName, role) VALUES ('U1410001', 'admin', 'Black', 'Sabbath', 0)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410000', 'admin', 'Foo', 'Fighters', 0)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410001', 'admin', 'Black', 'Sabbath', 0)
 
 -- Librarians
-INSERT INTO lms.Users(userId, password, firstName, lastName, role) VALUES ('U1510000', 'librarian', 'Led', 'Zeppelin', 2)
-INSERT INTO lms.Users(userId, password, firstName, lastName, role) VALUES ('U1510001', 'librarian', 'Judas', 'Priest', 2)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510000', 'librarian', 'Led', 'Zeppelin', 2)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510001', 'librarian', 'Judas', 'Priest', 2)
