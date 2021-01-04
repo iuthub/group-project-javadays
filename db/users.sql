95098813522613be16200133626effb22b464fb8
Mukhammadsaid19 <place2play2000@gmail.com>
2021-01-04 21:50:25 +0500

    Added NavBar with Styling

--- db/users.sql
+++ db/users.sql
@@ -11,13 +11,13 @@ CREATE TABLE Users(
 );
 
 -- Students
-INSERT INTO Users (UserID, Password, FirstName, LastName, Role) VALUES ('U1910223', '1234', 'Mukhammadsaid', 'Mamasaidov', 0)
-INSERT INTO Users (UserID, Password, FirstName, LastName, Role) VALUES ('U1910236', '1234', 'Jasur', 'Yusupov', 0)
+INSERT INTO Users (UserID, Password, FirstName, LastName, Role) VALUES ('U1910223', '1234', 'Mukhammadsaid', 'Mamasaidov', 0);
+INSERT INTO Users (UserID, Password, FirstName, LastName, Role) VALUES ('U1910236', '1234', 'Jasur', 'Yusupov', 0);
 
 -- Admins
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410000', 'admin', 'Foo', 'Fighters', 0)
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410001', 'admin', 'Black', 'Sabbath', 0)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410000', 'admin', 'Foo', 'Fighters', 0);
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410001', 'admin', 'Black', 'Sabbath', 0);
 
 -- Librarians
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510000', 'librarian', 'Led', 'Zeppelin', 1)
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510001', 'librarian', 'Judas', 'Priest', 1)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510000', 'librarian', 'Led', 'Zeppelin', 1);
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510001', 'librarian', 'Judas', 'Priest', 1);
