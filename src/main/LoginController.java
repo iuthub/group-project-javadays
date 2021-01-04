e3258da7472ec36b977e177bea572a46d3cf918d
Jasur Yusupov <j.yusupov@student.inha.uz>
2021-01-04 05:17:36 +0500

    Changes: - little update to users table - renewed authenticate function with alert logic - Alert label for GUI

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -2,32 +2,23 @@ package main;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Locale;
 
 public class LoginController {
-    @FXML
-    public BorderPane loginBorderPane;
+    @FXML private Label lblAlert;
+    @FXML private BorderPane loginBorderPane;
+    @FXML private TextField txtLogin;
+    @FXML private PasswordField txtPassword;
 
     @FXML
-    public Text h1;
-
-    @FXML
-    private TextField txtLogin;
-
-    @FXML
-    private PasswordField txtPassword;
-
-    @FXML
-    public void authenticate() {
-
+    public void authenticate_old() {
         // Creates Dialog with Buttons
         Dialog<ButtonType> dialog = new Dialog<>();
 
@@ -66,4 +57,29 @@ public class LoginController {
         // Waiting until user's action is performed
         dialog.showAndWait();
     }
+
+    public void authenticate(){
+        String login = txtLogin.getText();
+        String password = txtPassword.getText();
+        boolean status = false;
+
+        if (!login.equals("") && !password.equals("")){
+            if (login.matches("[uU][\\d]{7}")){
+                try {
+                    status = UsersRepository.getInstance().authenticate(login, password);
+                    if (!status) lblAlert.setText("Incorrect login or password!");
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                    lblAlert.setText(String.format("Database Error: %d", throwables.getErrorCode()));
+                }
+            } else {lblAlert.setText("Login pattern violation!");}
+
+        } else {lblAlert.setText("Not all fields are filled!"); }
+
+        if (status){
+            lblAlert.setTextFill(Color.GREEN);
+            lblAlert.setText("Success!");
+        }
+    }
+
 }
--- src/main/MainApplication.java
+++ src/main/MainApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
@@ -11,6 +12,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("loginDialog.fxml"));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/main/resources/favicon.png")));
         primaryStage.setTitle("Inha Library");
         primaryStage.setResizable(false);
         Scene scene = new Scene(root, 960, 540);
