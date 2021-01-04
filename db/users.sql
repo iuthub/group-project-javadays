f5d787714252d1e66b9f5028695ad445bcc64d8b
Jasur Yusupov <j.yusupov@student.inha.uz>
2021-01-04 23:01:27 +0500

    Added: - Generated 2000+ users, saved insert command to gen_users.sql - Updated create_db.sql - Finished sketch of StudentView GUI window - Implemented table view display of users in studentView window - Added some icons

--- db/users.sql
+++ db/users.sql
@@ -1,21 +1,22 @@
 DROP TABLE Users;
 
 CREATE TABLE Users (
-       userId      VARCHAR(8)  NOT NULL ALWAYS AS IDENTITY,
+       userId      VARCHAR(8)  NOT NULL,
        password    VARCHAR(20) NOT NULL,
        firstName   VARCHAR(40) NOT NULL,
        lastName    VARCHAR(40) NOT NULL,
-       role        INT         NOT NULL
-)
+       role        INT         NOT NULL,
+       PRIMARY KEY (userId)
+);
 
 -- Admins
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410000', 'admin', 'Foo', 'Fighters', 0)
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410001', 'admin', 'Black', 'Sabbath', 0)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410000', 'admin', 'Foo', 'Fighters', 0);
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1410001', 'admin', 'Black', 'Sabbath', 0);
 
 -- Librarians
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510000', 'librarian', 'Led', 'Zeppelin', 1)
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510001', 'librarian', 'Judas', 'Priest', 1)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510000', 'librarian', 'Led', 'Zeppelin', 1);
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1510001', 'librarian', 'Judas', 'Priest', 1);
 
 -- Students
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1910000', 'student', 'Pink', 'Floyd', 2)
-INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1910001', 'student', 'Deep', 'Purple', 2)
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1910000', 'student', 'Pink', 'Floyd', 2);
+INSERT INTO Users(userId, password, firstName, lastName, role) VALUES ('U1910001', 'student', 'Deep', 'Purple', 2);
--- src/main/java/StudentViewController.java
+++ src/main/java/StudentViewController.java
@@ -1,4 +1,54 @@
 package main.java;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class StudentViewController {
+    @FXML public TableView<Result> table;
+    private final PreparedStatement getAllStmt;
+    private final PreparedStatement getTotalCountStmt;
+    @FXML private Label total_count;
+
+    public StudentViewController() throws SQLException {
+        Connection conn = ConnectionManager.getConnection();
+        getAllStmt = conn.prepareStatement("SELECT userId, firstName || ' ' || lastName AS name FROM Users");
+        getTotalCountStmt = conn.prepareStatement("SELECT count(*) FROM Users");
+    }
+
+    public ObservableList<Result> getAll() throws SQLException{
+        ResultSet result;
+        ObservableList<Result> list = FXCollections.observableArrayList();
+
+        result = this.getAllStmt.executeQuery();
+
+        while (result.next()){
+            list.add(
+                new Result(
+                    result.getString("userId"),
+                    result.getString("name")
+                )
+            );
+        }
+        return list;
+    }
+
+    public void initialize(){
+        try {
+            ResultSet r = getTotalCountStmt.executeQuery();
+            r.next();
+            this.total_count.setText(String.format("Students count: %d", r.getInt(1)));
+            this.table.setItems(this.getAll());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
 }
