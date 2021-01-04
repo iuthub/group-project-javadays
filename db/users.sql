594fb8b7dcaa14f0b03032a582a08d34efce6f31
Jasur Yusupov <j.yusupov@student.inha.uz>
2021-01-04 14:40:01 +0500

    - corrected few mistakes in sql file

--- db/users.sql
+++ db/users.sql
@@ -5,9 +5,14 @@ CREATE TABLE Users(
     Password  VARCHAR(30) NOT NULL,
     FirstName VARCHAR(30) NOT NULL,
     LastName  VARCHAR(30) NOT NULL,
-    Role      INT         NOT NULL
+    Role      INT         NOT NULL,
+
+    PRIMARY KEY (UserID)
 );
 
 INSERT INTO Users (UserID, Password, FirstName, LastName, Role)
 VALUES ('U1910223', '1234', 'Mukhammadsaid', 'Mamasaidov', 0),
        ('U1910236', '1234', 'Jasur', 'Yusupov', 0),
+       ('U0000000', '0000', 'admin', 'admin', 0),
+       ('U1111111', '1111', 'lib', 'lib', 1),
+       ('U1234567', '1234', 'Marat', 'Shpagat', 2);
