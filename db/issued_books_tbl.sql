3a56d82b0f733f17019ea9602b3d9526b91463f2
Asadbek Khasanov <asadbek1khasanov@gmail.com>
2021-01-06 14:36:27 +0500

    added issued_books.sql and made some naming changes

---
+++ db/issued_books_tbl.sql
@@ -0,0 +1,26 @@
+-- UNTESTED --
+
+-- DROP TABLE IssuedBooks;
+
+CREATE TABLE IssuedBooks (
+     BookID          INT  NOT NULL REFERENCES Books(BookID),
+     UserID          VARCHAR(8)  NOT NULL REFERENCES Users(UserID),
+     IssueDate       DATE NOT NULL,
+     ReturnDate      DATE,
+
+     PRIMARY KEY (BookID,UserID)
+);
+
+/*
+INSERT INTO IssuedBooks(BookID, UserID, IssueDate, ReturnDate)
+VALUES
+('0001', 'U1910000', '01.01.2020', ''),
+('0001', 'U1910000', '01.01.2020', ''),
+('0002', 'U1910000', '01.01.2020', ''),
+('0002', 'U1910000', '01.01.2020', ''),
+('0003', 'U1910001', '01.01.2020', ''),
+('0003', 'U1910001', '01.01.2020', ''),
+('0004', 'U1910001', '01.01.2020', ''),
+('0004', 'U1910001', '01.01.2020', '');
+*/
+-- UNTESTED --
