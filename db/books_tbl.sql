c789c70ed3cad4f6919378fce83e3afaaa007fbd
Jasur Yusupov <j.yusupov@student.inha.uz>
2021-01-06 22:23:09 +0500

    Generated programming related books

--- db/books_tbl.sql
+++ db/books_tbl.sql
@@ -3,12 +3,13 @@
 DROP TABLE Books;
 
 CREATE TABLE Books (
-   BookID      INT             NOT NULL,
+   BookID      INT             NOT NULL GENERATED ALWAYS AS IDENTITY,
    ISBN        VARCHAR(20)     NOT NULL,
-   Title       VARCHAR(50)     NOT NULL,
+   Title       VARCHAR(60)     NOT NULL,
    Subject     VARCHAR(100)    NOT NULL,
    Author      VARCHAR(100)    NOT NULL,
    PublishDate DATE            NOT NULL,
+   Description VARCHAR(255),
 
    PRIMARY KEY(BookID)
 );
--- db/create_db.sql
+++ db/create_db.sql
@@ -4,3 +4,4 @@ RUN 'users_tbl.sql';
 RUN 'books_tbl.sql';
 RUN 'issued_books_tbl.sql';
 RUN 'gen_users.sql';
+RUN 'gen_books.sql';
